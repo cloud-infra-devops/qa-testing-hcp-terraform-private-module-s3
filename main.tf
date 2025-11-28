b757e1e063b77ae784f61f6a4519cd7b6818cf55
Pratik Das <pratik.x.das@gmail.com>
2025-11-28 01:24:31 -0600

    updated module version to 0.0.9

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ provider "aws" {
 
 module "s3" {
   source        = "app.terraform.io/cloud-infra-dev/s3/aws"
-  version       = "0.0.8"
+  version       = "0.0.9"
   bucket_prefix = "374278-s3-bucket"
 }
