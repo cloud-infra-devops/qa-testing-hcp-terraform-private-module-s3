45a80d791884dd7b76902bf52d9cb488834fe343
Pratik Das <pratik.x.das@gmail.com>
2025-11-28 08:48:39 -0600

    updated module version to 0.1.1

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ provider "aws" {
 
 module "s3" {
   source         = "app.terraform.io/cloud-infra-dev/s3/aws"
-  version        = "0.1.0"
+  version        = "0.1.1"
   s3_name_prefix = "374278-s3-bucket"
+  enable_kms     = true
 }
